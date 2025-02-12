f2dbe077f4cabb17fba3a4741c08fdbb9033fdeb
GalaxyBook4Pro <sprosj@naver.com>
2025-02-12 16:17:41 +0900

    연습용 초기버전

--- SimpleList.c
+++ SimpleList.c
@@ -1,3 +1,85 @@
 //
 // Created by spros on 25. 2. 11.
 //
+
+#include "SimpleList2.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void CopyToNode(struct movie item, struct node* pnode) {
+    pnode->item = item;
+}
+void InitializeList(struct list* plist) {
+    plist->head = NULL;
+}
+bool IsEmpty(const struct list* plist) {
+    if (plist->head == NULL)
+        return true;
+    else
+        return false;
+}
+bool IsFull(const struct list* plist) {
+    struct node* pt;
+    bool full;
+    pt = (struct node*)malloc(sizeof(struct node));
+    if (pt == NULL)
+        full = true;
+    else
+        full = false;
+    free(pt);
+    return full;
+}
+unsigned int CountItems(const struct list* plist) {
+    unsigned int count = 0;
+    struct node* pnode = plist->head;
+    while (pnode != NULL) {
+        ++count;
+        pnode = pnode->next;
+    }
+    return count;
+}
+bool InsertItemFront(struct movie item, struct node* prev) {
+    struct node* new_node;
+    new_node = (struct node*)malloc(sizeof(struct node));
+    if (new_node == NULL) {
+        printf("Malloc failed.\n");
+        return false;
+    }
+    CopyToNode(item, new_node);
+    struct node* temp = prev->next;
+    prev->next = new_node;
+    new_node->next = temp;
+    return true;
+}
+bool AddItem(struct movie item, struct list* plist) {
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    struct node* search = plist->head;
+    if (new_node == NULL) {
+        printf("Malloc failed.\n");
+        return false;
+    }
+    CopyToNode(item, new_node);
+    new_node->next = NULL;
+
+    if (search == NULL) {
+        plist->head = new_node;
+    }else {
+        while (search->next != NULL)
+            search = search->next;
+        search->next = new_node;
+    }
+    return true;
+}
+
+unsigned int WriteAllItems(const struct list* )
+
+
+
+
+
+
+
+
+
+
--- SimpleList.h
+++ SimpleList.h
@@ -5,4 +5,45 @@
 #ifndef SIMPLELIST_H
 #define SIMPLELIST_H
 
+#include <stdio.h>
+#include <stdbool.h>
+
+#define TSIZE 45
+
+struct movie {
+    char title[TSIZE];
+    float rating;
+};
+struct node {
+    struct movie item;
+    struct node* next;
+};
+struct list {
+    struct node* head;
+    int size;
+};
+
+void InitializeList(struct list* plist);
+bool IsEmpty(const struct list* plist);
+bool IsFull(const struct list* plist);
+bool AddItem(struct movie item, struct list* plist);
+void InsertByIndex(struct movie item, struct list* plist, int index);
+void RemoveByIndex(struct list* plist, int index);
+bool InsertItemFront(struct movie item, struct list* plist);
+bool Find(const struct list* plist, struct movie item_to_find, int* index, struct movie* item_found);
+bool (*compare_func)(struct movie a, struct movie b);
+
+unsigned int CountItems(const struct list* plist);
+unsigned int PrintAllItems(const struct list* plist, void (*print_an_item_func)(struct movie item));
+unsigned int WriteAllItems(const struct list* plist, FILE* file, void (*write_an_item_func)(FILE* file, struct movie item));
+
+void Traverse(const struct list* plist, void (*pfun)(struct movie item));
+void CLearList(struct list* plist);
+
+bool FindItemByIndex(const struct list* plist, const int index, struct movie** item);
+void RemoveFisrtItem(struct list* plist);
+
+
+
+
 #endif //SIMPLELIST_H
