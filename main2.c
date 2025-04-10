f02db6e824ee20cdaf3de4b23f21161400fb20f1
GalaxyBook4Pro <sprosj@naver.com>
2025-04-10 18:15:32 +0900

    main

--- main2.c
+++ main2.c
@@ -11,13 +11,16 @@ struct item {
 };
 
 void print_items_array(struct item items[], int size);
-int compare_items(vonst void* first, const void* second);
+int compare_items(const void* first, const void* second);
 
 const struct item* sequential_search(const struct item items[], const int size, const char name[TMAX]);
 const struct item* binary_search(const struct item items[], const int size, const char name[TMAX]);
 
+
 int main(void) {
-    struct item items[] = {
+
+    // 중복되는 키는 없다고 가정
+    struct item items[] ={
         {"Iron Man", "Tony Stark"},
         {"Thor", "Thor Odinson"},
         {"Ant-Man", "Hank Pym"},
@@ -31,7 +34,8 @@ int main(void) {
         {"Punisher", "Frank Castle"},
         {"Batman", "Bruce Wayne"}
     };
-        int n = sizeof(items) / sizeof(items[0]);
+
+    int n = sizeof(items) / sizeof(items[0]);
 
     print_items_array(items, n);
 
@@ -44,13 +48,14 @@ int main(void) {
         printf("Input key >> ");
 
         fgets(key, TMAX, stdin);
-        if ((strlen(key) >0) && (key[strlen(key) -1] == '\n'))
+        if ((strlen(key) > 0 ) && (key[strlen(key) -1] == '\n'))
             key[strlen(key) -1] = '\0';
-        if (strcmp(key, "exit") == 0)
+
+        if (strcmp(key, "exit")== 0)
             break;
 
-        const struct item* found = sequential_search(items, n, key);
-        found = binary_search(items, n, key);
+        // const struct item* found = sequential_search(items, n, key);
+        const struct item* found = binary_search(items, n, key);
 
         if (found == NULL)
             printf("Not in the list.\n\n");
@@ -58,6 +63,7 @@ int main(void) {
             printf("%s is %s\n\n", found->character, found->name);
 
     }
+
     return 0;
 }
 
@@ -76,26 +82,27 @@ int compare_items(const void* first, const void* second) {
 }
 
 const struct item* sequential_search(const struct item items[], const int size, const char key[TMAX]) {
-    for (int i = 0; i< size; ++i)
-        if (strcmp(items[i].character, key) ==0)
+    for (int i = 0; i < size; ++i)
+        if (strcmp(items[i].character, key) ==0 )
             return &items[i];
+
     return NULL;
 }
 
 const struct item* binary_search(const struct item items[], const int size, const char key[TMAX]) {
     int first = 0;
-    int last = size - 1;
+    int last = size -1;
 
-    while (first <= last) {
-        int middle = (first + last) /2;
+    while  (first <= last) {
+        int middle = (first + last) /2 ;
         int comp = strcmp(items[middle].character, key);
-        if (comp <0)
-            first = middle +1;
-        else if (comp ==0)
+        if (comp < 0 ) // 앞(mid)이 뒤(key)보다 작으면 left - mid - key - right
+            first = middle + 1;
+        else if (comp == 0)
             return &items[middle];
-        else
-            last = middle -1;
-
+        else   // left - key - mid - right
+            last = middle - 1;
     }
     return NULL;
+
 }
