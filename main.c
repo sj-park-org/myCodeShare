5665189aedca0b2c4ec2e94369f1ffdda131c740
my_room_workPC <sprosj@naver.com>
2025-05-09 11:16:06 +0900

    test

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef char data;
+typedef struct Node {
+    char key;
+    struct Node* left;
+    struct Node* right;
+}Node;
+
+Node* searchBST(Node *root, char x) {
+
+}
 
 int main(void)
 {
-
-
-
-
-
-
-
 
 
 
