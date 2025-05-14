d4e70387c7fce4e5b24347af3c18348e1c23c294
my_room_workPC <sprosj@naver.com>
2025-05-14 22:15:58 +0900

    test

--- main.c
+++ main.c
@@ -21,5 +21,28 @@ Node* searchBST(Node* root, char x ) {
     return NULL;
 }
 Node* insertBST(Node *root, char x) {
-
+    Node *p = root;
+    Node *parent = NULL;
+    while (p!= NULL) {
+        parent = p;
+        if (p->key == x) {
+            printf("같은 키가 있습니다.\n");
+            return p;
+        }
+        else if (p->key < x)
+            p= p->right;
+        else
+            p= p->left;
+    }
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->key = x;
+    newNode->left = newNode->right = NULL;
+    if (parent != NULL) {
+        if (parent->key < newNode->key)
+            parent->right = newNode;
+        else
+            parent->left = newNode;
+    }
+    return newNode;
 }
+
