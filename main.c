7e446a38328c6f8994e35a40452d03d6dd1544db
my_room_workPC <sprosj@naver.com>
2025-05-16 14:12:42 +0900

    test

--- main.c
+++ main.c
@@ -45,4 +45,74 @@ Node* insertBST(Node *root, char x) {
     }
     return newNode;
 }
+Node* deleteBST(Node *root, char x) {
+    Node* p = root;
+    Node* parent = NULL;
+    while ((p != NULL) && (p->key != x)) { //x와 일치하는 p값 찾기
+        parent = p;
+        if (p->key < x)
+            p=p->right;
+        else
+            p=p->left;
+    }
+    if (p==NULL) {
+        printf("찾는 노드가 없습니다.\n");
+        return root;
+    }
+    if (p->left == NULL && p->right == NULL) {
+        if (parent != NULL) {//부모가 NULL이 아닐 경우
+            if (parent->left == p)
+                parent->left = NULL; // 부모와 p의 연결해제
+            else
+                parent->right = NULL; // 부모와 p의 연결해제
+        }
+    }
+    else if (p->left == NULL || p->right == NULL) {
+        Node* child = (p->left != NULL) ? p->left : p->right; //1차수 조건 노드의 자식 찾기.
+        if (parent != NULL) {
+            if (parent->left == p)
+                parent->left = child;
+            else
+                parent->right = child;
+        }
+    }
+    else {
+        Node* succ_parent = p;
+        Node* succ = p->right;
+        while (succ->left != NULL) {
+            succ_parent = succ;
+            succ = succ->left;
+        }
+        p->key = succ->key;
+        if (succ_parent->left == succ)
+            succ_parent->left = succ->right;
+        else
+            succ_parent->right = succ->right;
+        p=succ;
+    }
+    free(p);
+    return root;
+}
 
+void Inorder(Node* root) {
+    if (root == NULL)
+        return;
+    Inorder(root->left);
+    printf("%c ", root->key);
+    Inorder(root->right);
+}
+
+int main(void)
+{
+    Node *root = insertBST(NULL, 'D');
+    insertBST(root, 'I');
+    insertBST(root, 'F');
+    insertBST(root, 'A');
+    insertBST(root, 'G');
+    insertBST(root, 'C');
+    Inorder(root); printf("\n");
+
+    root = deleteBST(root, 'C');
+    Inorder(root); printf("\n");
+    return 0;
+}
--- main2.c
+++ main2.c
@@ -51,7 +51,7 @@ Node* insertBST(Node *root, char x) {
 Node* deleteBST(Node *root, char x) {
     Node *p = root;
     Node *parent = NULL; //루트가 빈 경우
-    while ((p != NULL) && (p->key !=x)){
+    while ((p != NULL) && (p->key !=x)){//x와 일치하는 p값 찾기
         parent = p;
         if (p->key < x)
             p=p->right;
@@ -62,16 +62,16 @@ Node* deleteBST(Node *root, char x) {
         printf("찾는 노드가 없습니다.\n");
         return root;
     }
-    if (p->left == NULL && p->right == NULL) { //차수가 0
-        if (parent == NULL){
+    if (p->left == NULL && p->right == NULL) {
+        if (parent != NULL) {//부모가 NULL이 아닐 경우
             if (parent->left == p)
-                parent->left = NULL;
+                parent->left = NULL; // 부모와 p의 연결해제
             else
-                parent->right = NULL;
+                parent->right = NULL; // 부모와 p의 연결해제
         }
     }
-    else if (p->left == NULL || p->right == NULL) {
-        Node* child = (p->left != NULL) ? p->left : p->right;
+    else if (p->left == NULL || p->right == NULL) { //차수가 1
+        Node* child = (p->left != NULL) ? p->left : p->right; //1차수 조건 노드의 자식 찾기.
         if (parent != NULL){
             if (parent->left == p)
                 parent->left = child;
@@ -82,9 +82,9 @@ Node* deleteBST(Node *root, char x) {
     else {
         Node *succ_parent = p; //p 삭제할 값.(정확히는 값을 대체)
         //succ_parent succ의 부모 값. - 나중에 succ의 NULL 처리를 위한
+        // succ 우측서브트리에서 가장 작은 값
         //값을 대체하므로 부모 노드 ,자식 노드 연결을 수정할 필요 없음.
         Node *succ = p->right; // p->right, 우측 서브트리의 시작 값.
-        // succ 우측서브트리에서 가장 작은 값
         while (succ->left != NULL) { //가장 작은 값 찾기.
             succ_parent = succ; // succ p(삭제할 값) 하위 노드의 가장 작은 값.
             succ = succ->left; // succ_parent, 가장 작은 값의 부모 값.
@@ -93,11 +93,14 @@ Node* deleteBST(Node *root, char x) {
         if (succ_parent->left == succ)
             succ_parent->left = succ->right; //부모노드의 연결방향에 찾은 노드의 자식을 재결합
         else
-            succ_parent->left = succ->right;//부모노드의 연결방향에 찾은 노드의 자식을 재결합
+            succ_parent->right = succ->right;
+        //succ_parent가 p일 경우에는 succ_parent의 right에 succ가 존재함.
+        //p 삭제할값, succ 가장 작은값, p succ의 부모 . 이러한 관계.
+        //부모노드의 연결방향에 찾은 노드의 자식을 재결합
         p = succ;
     }
 
-    free(p);
+    free(p); // 차수 0,1,2의 p노드를 마지막으로 삭제
     return root;
 }
 
