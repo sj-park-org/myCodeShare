ff30bf28c3e51ab128ae33bbdf968decf65216d4
my_room_workPC <sprosj@naver.com>
2025-05-26 19:11:43 +0900

    test

--- main.c
+++ main.c
@@ -1,12 +1,39 @@
 #include "BinaryTreeArray.h"
 
+
+void preorder(Node root) {
+    if (isTreeEmpty(root))
+        return;
+    printf("%c ", getCurData(root));
+    preorder(getLeftChild(root));
+    preorder(getRightChild(root));
+}
+void inorder(Node root) {
+    if (isTreeEmpty(root))
+        return;
+    inorder(getLeftChild(root));
+    printf("%c ", getCurData(root));
+    inorder(getRightChild(root));
+}
+void postorder(Node root) {
+    if (isTreeEmpty(root))
+        return;
+    postorder(getLeftChild(root));
+    postorder(getRightChild(root));
+    printf("%c ", getCurData(root));
+
+}
+
 void main() {
-    Node n1 = makeRoot('-');
-    Node n2 = makeLeftChild(n1, '*');
-    Node n3 = makeRightChild(n1, '/');
-    Node n4 = makeLeftChild(n2, 'A');
-    Node n5 = makeRightChild(n2, 'B');
-    Node n6 = makeLeftChild(n3, 'C');
-    Node n7 = makeRightChild(n3, 'D');
+    Node _a = makeRoot('A');
+    Node _b = makeLeftChild(_a, 'B');
+    Node _c = makeRightChild(_a, 'C');
+    Node _d = makeLeftChild(_b, 'D');
+    Node _e = makeRightChild(_b, 'E');
+    Node _f = makeLeftChild(_c, 'F');
+    Node _g = makeRightChild(_c, 'G');
+
+    postorder(_a); printf("\n");
+
 
 }
